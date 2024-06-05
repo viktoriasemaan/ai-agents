ce150a0ecc9d5455486af260ba55f2a76937e2a4
Viktor Vedmich <victor.vedmich@gmail.com>
2024-06-05 17:52:46 +0000

    Refactor changes

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM infracost/infracost:ci-latest as infracost
 # Use the amazon/aws-lambda-python:3.11 as the base image
 FROM amazon/aws-lambda-python:3.11
 
-# Install tar package (assuming it's not already installed)
+# Install the tar package (assuming it's not already installed)
 RUN yum install -y tar
 
 # Create a directory for the infracost binary
@@ -16,16 +16,17 @@ COPY --from=infracost /usr/bin/infracost /app/
 # Set the PATH environment variable to include the directory containing the infracost binary
 ENV PATH="/app:${PATH}"
 
-# Install deps
+# Copy the requirements file and install dependencies
 COPY requirements.txt ${LAMBDA_TASK_ROOT}
 
+# Upgrade pip and install required Python packages
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 RUN pip install -U boto3 botocore
 
-# Copy function code
+# Copy the function code
 COPY index.py ${LAMBDA_TASK_ROOT}
 COPY tools.py ${LAMBDA_TASK_ROOT}
 
-# Set the CMD to your handler
+# Set the CMD to the Lambda handler
 CMD [ "index.handler" ]
--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import image_to_text as tools  # This import is correct
 
-st.title(f""":rainbow[Your Solutions Architect Assistant powered by Amazon Bedrock]""")
+# Set the title of the Streamlit app
+st.title(":rainbow[Your Solutions Architect Assistant powered by Amazon Bedrock]")
 
-st.header(f""":rainbow[Explanation and Code generated using Claude 3 Sonnet]""")
+# Set the header of the Streamlit app
+st.header(":rainbow[Explanation and Code generated using Claude 3 Sonnet]")
+
+# Create a container for file upload and user input
 with st.container():
     st.subheader('Image File Upload:')
-    File = st.file_uploader('Upload an Image', type=["png", "jpg", "jpeg"], key="diag")
-    if File is not None:
-        st.image(File, caption='Uploaded Image', use_column_width=True)
+    file = st.file_uploader('Upload an Image', type=["png", "jpg", "jpeg"], key="diag")
+    
+    # Display the uploaded image if there is one
+    if file is not None:
+        st.image(file, caption='Uploaded Image', use_column_width=True)
+    
+    # Input field for an optional question
     text = st.text_input("OPTIONAL: Do you have a question about the image? Or about anything in general?")
+    
+    # Buttons to trigger actions
     result1 = st.button("Explain Diagram")
     result2 = st.button("Generate Terraform")
-    
+
+# Set the input text based on the button pressed
 if result1:
     input_text = "You are an AWS Solutions Architect. The image you've received is an architecture diagram. Please explain the technical data flow in detail, step-by-step, and identify each AWS service used in the diagram."
 elif result2:
@@ -20,13 +31,11 @@ elif result2:
 else:
     input_text = ""
 
-# Ensure we only call the function if a file is uploaded and a button is pressed
-if File is not None and (result1 or result2):
+# Ensure the function is called only if a file is uploaded and a button is pressed
+if file is not None and (result1 or result2):
     # Use the 'tools' module to call 'image_to_text'
-    # Temporarily saving the uploaded file to pass its path to your function
-    with open(File.name, "wb") as f:
-        f.write(File.getbuffer())
-    st.write(tools.image_to_text(File.name, input_text))
-
-
-
+    # Temporarily save the uploaded file to pass its path to the function
+    with open(file.name, "wb") as f:
+        f.write(file.getbuffer())
+    # Call the image_to_text function with the file path and input text
+    st.write(tools.image_to_text(file.name, input_text))
